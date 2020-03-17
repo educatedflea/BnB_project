e7909dac715cabf2d1a7e9a02509e10182d5a528
Student <student@makerss-air-2.home>
2020-03-17 12:35:13 +0000

    test_homepage

--- spec/features/test_page_spec.rb
+++ spec/features/test_page_spec.rb
@@ -1,7 +1,12 @@
 feature 'Viewing test page' do
 	scenario 'visiting the test page' do
 		visit('/test')
-		expect(page).to have_content "Test page"
-  end
+    expect(page).to have_content "Test page"
+
   
+  end
+  scenario 'visit home page' do
+   visit('/')
+   expect(page).to have_content "ViralBnB"
+  end
 end
