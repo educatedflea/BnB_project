c08a64f03111d3a2b1b78a7da339f290f9e4fa5f
Olivia Thetgyi <othetgyi@gmail.com>
2020-03-20 09:55:31 +0000

    Can add renter name to database

--- lib/renter.rb
+++ lib/renter.rb
@@ -1,8 +1,9 @@
 require 'pg'
 
 class Renter
-  attr_reader :renter_name
-  def initialize(renter_name:)
+  attr_reader :id, :renter_name
+  def initialize(id:, renter_name:)
+    @id = id
     @renter_name = renter_name
   end
 
@@ -12,7 +13,6 @@ class Renter
 		else
 			connection = PG.connect(dbname: 'bnb')
     end
-
     result = connection.exec("SELECT * FROM renter;")
 		result.map do |renter|
 			Renter.new(renter_name: renter['renter_name'])
@@ -26,7 +26,7 @@ class Renter
        # "in the actual db"
       connection = PG.connect(dbname: 'bnb')
   end
-    result = connection.exec("INSERT INTO renter (renter_name) VALUES ('#{renter_name}');")
-    Renter.new(renter_name: result[0]['renter_name'])
+    result = connection.exec("INSERT INTO renter (renter_name) VALUES ('#{renter_name}') RETURNING renter_name;")
+    Renter.new(id: result[0]['id'], renter_name: result[0]['renter_name'])
   end
 end
