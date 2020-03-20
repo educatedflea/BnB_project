561e4cf5e88c92e200921e97992a5ff81d150843
Olivia Thetgyi <othetgyi@gmail.com>
2020-03-20 14:26:24 +0000

    Trying to change availability on database

--- lib/booking.rb
+++ lib/booking.rb
@@ -1,9 +1,11 @@
 require 'pg'
+require_relative 'rooms'
 
 class Booking
 
-  attr_reader :renter_name, :room_name, :date, :request_status
-  def initialize(renter_name:, room_name:, date:, request_status:)
+  attr_reader :renter_name, :room_name, :date, :request_status, :id
+  def initialize(id:, renter_name:, room_name:, date:, request_status:)
+    @id = id
     @renter_name = renter_name
     @room_name = room_name
     @date = date
@@ -18,7 +20,20 @@ def self.pending
   end
   request = connection.exec("SELECT * FROM bookingrequest;")
   request.map do |request|
-  Booking.new(renter_name: request['renter_name'], room_name: request['room_name'], date: request['date'], request_status: request['request_status'])
+  Booking.new(id: request['id'], renter_name: request['renter_name'], room_name: request['room_name'], date: request['date'], request_status: request['request_status'])
   end
+  end
+
+  def self.approve(id)
+
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bnb_test')
+    else
+      connection = PG.connect(dbname: 'bnb')
+  end
+  result = connection.exec("UPDATE rooms SET availability='Not available' WHERE id=#{id};")
+
+  # result = connection.exec
+  # ^^ this one changes bookingrequest status to approved
   end
 end
